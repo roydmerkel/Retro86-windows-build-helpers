a52655bdb52d57c5bbf53f41b257153e446aea39
Roy Merkel <merkel-roy@comcast.net>
2018-09-09 21:37:42 -0700

    Added max volume length test.

--- ndif_research/rundiff.sh
+++ ndif_research/rundiff.sh
@@ -7,3 +7,14 @@ export base=`pwd`
 gcc rforkdump.c -o rforkdump -O0 -g
 find . -type "f" -iname "*.rsrc" -exec bash -c 'cd `dirname "{}"`; ${base}/rforkdump `basename "{}"` > `basename "{}"`.hrfork' \;
 
+pushd images/ro/name
+diff -Naur A.img.rsrc.hrfork B.img.rsrc.hrfork
+diff -Naur B.img.rsrc.hrfork Bo.img.rsrc.hrfork
+diff -Naur Bo.img.rsrc.hrfork 123456789012345678901234567.img.rsrc.hrfork
+popd
+
+pushd images/rw/name
+diff -Naur A.img.rsrc.hrfork B.img.rsrc.hrfork
+diff -Naur B.img.rsrc.hrfork Bo.img.rsrc.hrfork
+diff -Naur Bo.img.rsrc.hrfork 123454678901234567890123456.img.rsrc.hrfork
+popd
